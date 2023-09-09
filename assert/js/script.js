16dc9ff9d685c0930061f81395b12cae832093c5
braa <Braashaban101@gmail.com>
2023-09-09 10:12:17 +0300

    task

--- assert/js/script.js
+++ assert/js/script.js
@@ -1,13 +1,59 @@
+
+
+
 const cc=document.querySelector('.navbar');
-const remov=document.querySelector('top-nav')
-window.addEventListener('scroll', () =>{
-    if (window.scrollY >= 56) {
-        cc.classList.add('navbar-scrool');
-        remov.classList.remove('top-nav')
-    }else{
-        cc.classList.remove('navbar-scrool')
+const addd=document.querySelector('.top-nav');
+
+window.addEventListener('scroll',() => {
+ /* تعريف متغير */
+  
+
+    var d = 60- window.scrollY;
+
+    if( window.scrollY < 60){
+    
+      cc.style='top : '+ d +'px !important'; 
     }
 
-}
-)
+    else if(window.scrollY >= 60){
+
+        /* عند السكرول لقيمة اضغر من60 اجعل قيمة توب 0 للناف  */ 
+        cc.style='top : '+ 0 +'px !important';  
+    }
+
+    /* اضافة الكلاس الذي يقوم بتفير الخلفية عند الوصول للقيمة 0 */
+    if(window.scrollY >= 60){
+        cc.classList.add('navbar-scrool');
+
+    } /*ايضا عند الوصول للقيمة اصغر من 60  يقوم باضافة الكلاس  الذي يحذف الديف الاول */
+    else if( window.scrollY < 60){
+        addd.classList.remove('top-nav');
+       
+    }
+
+})
+
+/* طريقة تانية بدون عداد لانقاص top */
+
+/* 
+
+window.addEventListener('scroll', () =>{
+    if (window.scrollY >= 60) {
+      
+        cc.classList.add('navbar-scrool');
+       
+       addd.classList.add('display');
     
+       cc.classList.add('toop');
+    };
+    else{
+        cc.classlist.remove('navbar-scrool')
+    }
+   })
+
+
+
+*/
+
+
+
